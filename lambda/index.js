aa64d42e49cb934f16500cb2adef61a23cd49c02
Jérémie Tarot (@silopolis) <silopolis@gmail.com>
2024-07-19 13:30:33 +0200

    Add support for ED25519 key generation to lambda function

--- lambda/index.js
+++ lambda/index.js
@@ -1,25 +1,46 @@
 const AWS = require('aws-sdk')
 const forge = require('node-forge')
 
-const rsa = forge.pki.rsa
-
 const secretsmanager = new AWS.SecretsManager()
 
+const keyType = process.env.KEY_TYPE || 'RSA'
 const bits = parseInt(process.env.KEY_BITS) || 2048
+
+async function generateRSAKeyPair() {
+  const {rsa} = forge.pki
+  return rsa.generateKeyPair({ bits, e: 0x10001 })
+}
+
+async function generateED25519KeyPair() {
+  const {ed25519} = forge.pki
+  const keypair = ed25519.generateKeyPair()
+
+  return {
+    publicKey: keypair.publicKey,
+    privateKey: keypair.privateKey
+  }
+}
 
 exports.handler = async (event, context) => {
   try {
-    const keypair = rsa.generateKeyPair({ bits, e: 0x10001 })
+    let keypair
+    if (keyType === 'ED25519') {
+      keypair = await generateED25519KeyPair()
+    } else {
+      keypair = await generateRSAKeyPair()
+    }
+
     await secretsmanager.putSecretValue({
       SecretId: event.pubkey_secret_name,
-      SecretString: forge.ssh.publicKeyToOpenSSH(keypair.publicKey)
+      SecretString: keyType === 'ED25519' ? keypair.publicKey.toString('base64') : forge.ssh.publicKeyToOpenSSH(keypair.publicKey)
     }).promise()
+
     await secretsmanager.putSecretValue({
       SecretId: event.privkey_secret_name,
-      SecretString: forge.ssh.privateKeyToOpenSSH(keypair.privateKey)
+      SecretString: keyType === 'ED25519' ? keypair.privateKey.toString('base64') : forge.ssh.privateKeyToOpenSSH(keypair.privateKey)
     }).promise()
   } catch (error) {
     console.error(error)
-    throw (error)
+    throw error
   }
 }
