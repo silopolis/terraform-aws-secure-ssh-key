899a157add66f0d0fe03f1d253ecbe497c936d4d
Jérémie Tarot (@silopolis) <silopolis@gmail.com>
2024-07-19 17:00:23 +0200

    Convert module from CommonJS to ES

--- lambda/index.js
+++ lambda/index.js
@@ -1,18 +1,18 @@
-const AWS = require('aws-sdk')
-const forge = require('node-forge')
+import { SecretsManager } from 'aws-sdk'
+import { pki, ssh } from 'node-forge'
 
-const secretsmanager = new AWS.SecretsManager()
+const secretsmanager = new SecretsManager()
 
 const keyType = process.env.KEY_TYPE || 'RSA'
 const bits = parseInt(process.env.KEY_BITS) || 2048
 
 async function generateRSAKeyPair() {
-  const {rsa} = forge.pki
+  const {rsa} = pki
   return rsa.generateKeyPair({ bits, e: 0x10001 })
 }
 
 async function generateED25519KeyPair() {
-  const {ed25519} = forge.pki
+  const {ed25519} = pki
   const keypair = ed25519.generateKeyPair()
 
   return {
@@ -21,7 +21,7 @@ async function generateED25519KeyPair() {
   }
 }
 
-exports.handler = async (event, context) => {
+export async function handler(event, context) {
   try {
     let keypair
     if (keyType === 'ED25519') {
@@ -32,12 +32,12 @@ exports.handler = async (event, context) => {
 
     await secretsmanager.putSecretValue({
       SecretId: event.pubkey_secret_name,
-      SecretString: keyType === 'ED25519' ? keypair.publicKey.toString('base64') : forge.ssh.publicKeyToOpenSSH(keypair.publicKey)
+      SecretString: keyType === 'ED25519' ? keypair.publicKey.toString('base64') : ssh.publicKeyToOpenSSH(keypair.publicKey)
     }).promise()
 
     await secretsmanager.putSecretValue({
       SecretId: event.privkey_secret_name,
-      SecretString: keyType === 'ED25519' ? keypair.privateKey.toString('base64') : forge.ssh.privateKeyToOpenSSH(keypair.privateKey)
+      SecretString: keyType === 'ED25519' ? keypair.privateKey.toString('base64') : ssh.privateKeyToOpenSSH(keypair.privateKey)
     }).promise()
   } catch (error) {
     console.error(error)
